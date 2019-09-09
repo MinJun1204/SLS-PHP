bf073c61989d0766f423677cd1a5ce255bd07dac
MinJun1204 <35185990+MinJun1204@users.noreply.github.com>
2019-09-09 15:20:42 +0900

    20190909

---
+++ jquery test.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>test</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<style>
+    #a{
+        border: 2px solid green;
+    }
+</style>
+</head>
+<body>
+    <div id="a">이 태그의 이름을 a라고 붙여준것 뿐이야</div>
+    <button id="hide">보이기</button>
+    <button id="show">숨기기</button>
+    <script>
+        $(document).ready(function(){
+            $("#hide").click(function(){
+                $("#a").hide();    
+            });
+            $("#show").click(function(){
+                $("#a").show();
+            });
+        });
+    </script>
+</body>
+</html>
--- process.php
+++ process.php
@@ -22,12 +22,31 @@
         button {
             background-color: white;
         }
+        
+        
+        input[type="checkbox"] {
+            /* display: none; */
+        }
+
+        .cspan {
+            display: inline-block;
+            text-align: center;
+            width: 30px;
+            height: 30px;
+            background-color: #DDD;
+            margin: 5px;    
+        }
+
+        .blue {
+            background-color: #5CD1E5;
+        }
     </style> 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
 
-<?php
+    <form action="./lend.php">
+    <?php
     // MySQL 서버 접속
 
     $user = $_POST['user']; 
@@ -38,7 +57,7 @@
 
     $conn = mysqli_connect($servername, $username, $password, $db);
     if (!$conn) {
-        die("데이터베이스 접속 실패: " . mysqli_connect_error() . '<hr><h1>관리자에게 문의하세요</h1>');
+        die("접속 실패: " . mysqli_connect_error() . '<hr><p>관리자에게 문의하세요</p>');
     }
     $query = "UPDATE `sls` SET `user` = '{$user}' WHERE `id` = 1";
     if (mysqli_query($conn, $query)) {
@@ -47,29 +66,32 @@
         echo 'Error updating record: ' . mysqli_error($conn);
     }
 
-    // 사용 중이 아닌 노트북 리스트 호출'
+    // 사용 중이 아닌 노트북 리스트 출력'
 
     echo '<table>';
     $query = 'SELECT `id` FROM `sls` WHERE `c1` = 0';
     $result = mysqli_query($conn, $query);
     if (mysqli_num_rows($result) > 0) {
+        $i = 0;
         while ($row = mysqli_fetch_assoc($result)) {
-            echo '<tr>' . '<td> <button class="btn" id="' . $row['id'] . '">' . $row['id'] . '</button></td>';
+            $i ++;
+            echo '<input type="checkbox" name="laptop[]" value="' . $i . '" id="c' . $i . '">';
+            echo '<label for="c' . $i . '">';
+            echo '<span class="cspan" id="s' . $i . '">' . $i . '</span>';
+            echo '</label>';
+        #    echo '<tr>' . '<td> <button class="btn" id="' . $row['id'] . '">' . $row['id'] . '</button></td>';
         }
     }
     echo '</table>'
     ?>
 
+    <input type="submit" value="대여">
+    </form>
     <script>
-        var count = 0;
         $(document).ready(function() {
-            $('.btn').click(function() {
-                count = count + 1;
-                if (count % 2 == 0) {
-                    $(this).css('background-color', 'white');
-                } else {
-                    $(this).css('background-color', '#5CD1E5');
-                }
+            $('.btn, .cspan').click(function(event) {
+                event.preventDefault();
+                $(this).toggleClass('blue');
             });
          });
     </script>
