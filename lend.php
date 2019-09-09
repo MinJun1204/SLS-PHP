6736ce583cd15271572ab2bd44b41f23b614f235
MinJun1204 <35185990+MinJun1204@users.noreply.github.com>
2019-09-09 17:36:20 +0900

    Make Lend System Query

--- lend.php
+++ lend.php
@@ -1,5 +1,14 @@
 <?php
 
-print_r($_GET['laptop'][3]);
+print_r($_GET['laptop']);
+echo '<br>';
+
+for($i=0;$i<sizeof($_GET['laptop']);$i++){
+    $id = $_GET['laptop'][$i];
+    $query="update sls set c1 = 1 where id=$id";
+    echo $query . '<br>';
+
+}
+
 
 ?>
